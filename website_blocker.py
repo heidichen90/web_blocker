5d64076fe51e7648ee2e1629e5200cc20bf2950e
Heidi Chen <toddypet@gmail.com>
2019-09-07 10:18:36 +0100

    amend the website_bloker.py

--- website_blocker.py
+++ website_blocker.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 host_temp = "hosts"
 host_path = "/etc/hosts"
 redirect = "127.0.0.1"
-website_list = ["www.facebook.com", "facebook.com"]
+website_list = ["facebook.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 19):
